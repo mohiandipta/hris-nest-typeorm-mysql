2539f50b0ff18a32b67fa6bec35a7c03efaa02de
mohiandipta <mohianmostofa@gmail.com>
2023-08-27 15:53:22 +0600

    feat: hello world restore

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { EmployeeModule } from './modules/employee/employee.module';
+import { AppService } from './app.service';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [EmployeeModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
