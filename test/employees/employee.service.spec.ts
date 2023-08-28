861f0a7eebc978e3847f83af1510fdfdaf033a1e
mohiandipta <mohianmostofa@gmail.com>
2023-08-28 10:29:58 +0600

    feat: test modified

--- test/employees/employee.service.spec.ts
+++ test/employees/employee.service.spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { EmployeeService } from './employee.service';
+import { EmployeeService } from '../../src/modules/employee/services/employee.service';
 
 describe('EmployeeService', () => {
   let service: EmployeeService;
