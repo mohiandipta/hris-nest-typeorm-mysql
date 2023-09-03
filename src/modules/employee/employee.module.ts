213b67b45322c30c91db39247b7a883f0ff6ee81
mohiandipta <mohianmostofa@gmail.com>
2023-09-03 12:04:28 +0600

    feat: db path modified

--- src/modules/employee/employee.module.ts
+++ src/modules/employee/employee.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { EmployeeService } from './services/employee.service';
 import { EmployeeController } from './controllers/employee.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../../database/database.module';
 
 @Module({
   imports: [DatabaseModule],
