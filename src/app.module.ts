aac59e09cc9691084629d5d7dfb2a2c37a0008e3
mohiandipta <mohianmostofa@gmail.com>
2023-09-03 12:42:14 +0600

    feat: db added into appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
 import { EmployeeModule } from './modules/employee/employee.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [EmployeeModule],
+  imports: [DatabaseModule, EmployeeModule],
   controllers: [AppController],
   providers: [AppService],
 })
