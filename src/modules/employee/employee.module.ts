60088404404175bf5c66c7499b9c3a7bd4644080
mohiandipta <mohianmostofa@gmail.com>
2023-09-03 10:53:22 +0600

    feat: error module removed

--- src/modules/employee/employee.module.ts
+++ src/modules/employee/employee.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 // import { EmployeeController } from './controllers/employee.controller';
 import { EmployeeService } from './services/employee.service';
 import { employeeProviders } from './providers/employee.provider';
-import { ErrorHandlingModule } from 'src/shared/error/error.module';
 import { EmployeeController } from './controllers/employee.controller';
 
 @Module({
-  imports: [ErrorHandlingModule],
+  imports: [],
   controllers: [EmployeeController],
   providers: [
     ...employeeProviders,
