6f66c3f43ae084f02ba63a19acf09f4f49034cd0
mohiandipta <mohianmostofa@gmail.com>
2023-09-03 10:34:34 +0600

    databse added

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { AppController } from './app.controller';
+import { EmployeeController } from './employee/employee.controller';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [DatabaseModule],
+  controllers: [AppController, EmployeeController],
   providers: [AppService],
 })
 export class AppModule {}
